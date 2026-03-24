634393b7261a3c172525b290b98a8a006b951c65
DevJoao-00 <victorjoao19921210@gmail.com>
2026-03-24 16:30:07 +0000

    5

--- fup/sobrou/go/draft.go
+++ fup/sobrou/go/draft.go
@@ -1,5 +1,21 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println( )
+    var p1 int
+    var p2 int
+    var p3 int 
+    var v1 float32
+    var v2 float32 
+    var v3 float32 
+    var qtddiero float32
+    fmt.Scan(&p1)
+    fmt.Scan(&p2)
+    fmt.Scan(&p3)
+    fmt.Scan(&v1)
+    fmt.Scan(&v2)
+    fmt.Scan(&v3)
+    fmt.Scan(&qtddiero)
+
+    var sobra float32 = qtddiero - float32(p1) * v1 + float32(p2) * v2 + float32(p3) * v3 
+    fmt.Printf("%.2f\n", sobra)
 }
