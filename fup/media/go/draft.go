a1fef8638a5bc709f450da9522022fd00bc1bd2f
DevJoao-00 <victorjoao19921210@gmail.com>
2026-03-24 17:23:41 +0000

    7

--- fup/media/go/draft.go
+++ fup/media/go/draft.go
@@ -1,6 +1,12 @@
 package main
 import "fmt"
 func main() {
-    var a, b int  
-    fmt.Printf(%)
+
+    var a, b int 
+
+    fmt.Scan(&a, &b) 
+
+    var media = float32(a + b) / 2
+
+    fmt.Printf("%.1f\n", media)
 }
