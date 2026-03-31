5fbba8605480caebdaf7ee6a97a9a61416759097
DevJoao-00 <victorjoao19921210@gmail.com>
2026-03-31 17:42:09 +0000

    14

--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -1,5 +1,12 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+)
 func main() {
-    fmt.Println("Hello, World!")
+    var x1,y1,x2,y2 float64
+    fmt.Scan(&x1,&y1,&x2,&y2)
+    dAB:= math.Sqrt(math.Pow(x2 - x1, 2) + math.Pow(y2 - y1, 2))
+    fmt.Printf("%.2f\n", dAB)
 }
