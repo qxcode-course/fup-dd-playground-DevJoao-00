27dd2a9c8854f66800845d031f55fdf7afb105dc
DevJoao-00 <victorjoao19921210@gmail.com>
2026-04-07 18:00:38 +0000

    wowww

--- fup/guarda/src/go/draft.go
+++ fup/guarda/src/go/draft.go
@@ -1,5 +1,20 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+
+    var wifi bool
+    var login bool
+    var admin bool
+
+    fmt.Scan(&wifi,&login,&admin)
+
+    if wifi == false{
+        fmt.Println("you must connect to wifi")
+    } else if login == false{
+        fmt.Println("you need to login first")
+    } else if admin == false{
+        fmt.Println("you must to login as admin")
+    } else {
+        fmt.Println("done")
+    }
 }
