b2c1f1afe3a1f16c15a95c2e58a33f13b599cf3a
DevJoao-00 <victorjoao19921210@gmail.com>
2026-04-07 13:48:29 +0000

    ta quaseee

---
+++ fup/operacoes/src/go/draft.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func main() {
+    var n1 int 
+    var n2 int
+
+    fmt.Scan(&n1, &n2)
+
+    soma := n1 + n2
+    sub := n1 - n2
+    mult := n1 * n2
+    div := n1 / n2
+    rest := n1 % n2
+
+    fmt.Println(soma)
+    fmt.Println(sub)
+    fmt.Println(mult)
+    fmt.Printf("%.2f\n", float32(div))
+    fmt.Println(rest)
+}
