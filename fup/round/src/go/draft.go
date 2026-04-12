bb05456fe9551eb3b0d1cc42fc8df4cba3b946e1
DevJoao-00 <victorjoao19921210@gmail.com>
2026-04-12 20:29:05 +0000

    aleluia

--- fup/round/src/go/draft.go
+++ fup/round/src/go/draft.go
@@ -1,19 +1,47 @@
 package main
 import "fmt"
+
+func ceil(nota float64) int {
+    inteiro := int(nota)
+
+    if nota > 0 && nota != float64(inteiro){
+        return inteiro + 1
+    }
+        return inteiro
+}
+func floor(nota float64) int {
+    inteiro := int(nota)
+
+    if nota < 0 && nota != float64(inteiro){
+        return inteiro - 1
+    }
+        return inteiro
+}
+func round(nota float64) int {
+    inteiro := int(nota)
+    partdecimal := nota - float64(inteiro)
+
+    if inteiro < 0 && partdecimal != 0{
+        partdecimal = float64 (inteiro) - nota
+    }
+    if partdecimal >= 0.5 {
+        return ceil(nota)
+    } else {
+        return floor(nota)
+    }
+        
+}
 func main() {
     var car string
-    var num float64
+    var nota float64
 
-    fmt.Scan(&car, &num)
+    fmt.Scan(&car, &nota)
 
-    inteiro := int(num)
-
-    if car == "c"{
-        if inteiro % 2 != 0{
-            if num >= 0.5{
-                fmt.Println(num)
-            }
-
-        }
+    if car == "c" {
+        fmt.Println(ceil(nota))
+    } else if car == "f" {
+        fmt.Println(floor(nota))
+    } else if car == "r" {
+        fmt.Println(round(nota))
     }
 }
