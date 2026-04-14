601c6df99118779d4d8ea2d7fa2576edb686dc3a
DevJoao-00 <victorjoao19921210@gmail.com>
2026-04-14 17:31:22 +0000

    aula massa

---
+++ sandbox/user_001/src/go/draft.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+func main() {
+    fmt.Print("hello, world")   
+    }
+
+    //var qtd int
+    //fmt.Scan(&qtd)
+    //arr := []int {4,5,6,7,8,9}
+        //fmt.Printf("[ ")
+        //for _, valor:= range arr{
+            //if i != 0{
+
+    //for i := 2; i < len(arr) - 2; i++{
+    //fmt.Printf("%d ", arr[i])
+    //fmt.Printf("%d, ", valor) 
+
+
+       // fmt.Printf("]\n")
+
+    //var idades []int = make([]int, qtd)
+   //var lista []int = []int {1,4,2,5,7}
+   //var nomes []string = []string {"João","Erik","Iago","Arisnaldo"}
+
+   //nomes = []string {"banana"," kiwi"}
+   //nomes[2] = "Senzala"
+   //nomes[3] = "Fup endoida eu"
+
+   //fmt.Println(lista)
+   //fmt.Println(nomes[2], nomes[3])
+   //fmt.Print(idades)
+
