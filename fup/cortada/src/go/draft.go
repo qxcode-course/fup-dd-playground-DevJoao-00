3e7abc253ab6411b01c2bead8069e8c0015e0154
DevJoao-00 <victorjoao19921210@gmail.com>
2026-04-19 21:29:13 +0000

    av credo

--- fup/cortada/src/go/draft.go
+++ fup/cortada/src/go/draft.go
@@ -5,4 +5,15 @@ func main() {
 
   fmt.Scan(&b,&t)
 
+  met := (160 * 70) / 2
+  areaF := ((b + t)/2) * 70
+
+  if areaF > met {
+    fmt.Println("1")
+  }else if areaF < met {
+    fmt.Println("2")
+  } else {
+    fmt.Println("0")
+  }
+
 }
--- fup/vet_i/src/go/draft.go
+++ fup/vet_i/src/go/draft.go
@@ -3,12 +3,17 @@ import "fmt"
 func main() {
     var N int
     fmt.Scan(&N)
-    var list []int = make([]int, N)
+
+    var list[]int = make([]int, N)
+
     for i := range list{      
-            fmt.Scan(&list[i])
+        fmt.Scan(&list[i])
     }
+
     for _, valor := range list{
         fmt.Println(valor)
     }
-
+        if N == 0{
+            fmt.Println("")
+        }
 }
