3118deb2739a6dd954d81f95bda49d6e73c86bb0
DevJoao-00 <victorjoao19921210@gmail.com>
2026-05-05 18:26:54 +0000

    amem

--- fup/game10/src/go/draft.go
+++ fup/game10/src/go/draft.go
@@ -1,5 +1,16 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Helo, World!")
+    var N, D, A int
+
+    fmt.Scan(&N, &D, &A)
+
+    if N < 3 || N > 100{
+        return
+    }
+
+    if D > A {
+        A = (A + 1) % N
+    }
+    fmt.Println(A)
 }
