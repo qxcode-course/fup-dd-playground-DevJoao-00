7a10015da072c53e3fdb5f1e484717b78447d43f
DevJoao-00 <victorjoao19921210@gmail.com>
2026-05-14 19:51:37 +0000

    .

--- fup/fuga/src/go/draft.go
+++ fup/fuga/src/go/draft.go
@@ -4,4 +4,24 @@ func main() {
     var H, P, F, D int
 
     fmt.Scan(&H, &P, &F, &D)
+
+    dif := H - P
+
+    if H >= 0 && H < 16 && P >= 0 && P < 16 && F >= 0 && F < 16 && H != P && H != F && P != H{
+       for H != F{
+            if F > dif {
+                F = (F - D) % 16
+                fmt.Println("S") 
+                break
+            } 
+
+        }
+            if F < dif {
+                F = P
+                fmt.Println("N")
+                
+        }   
+    } else {
+        return
+    }
 }
