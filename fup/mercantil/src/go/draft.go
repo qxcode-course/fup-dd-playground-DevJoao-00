a202a2bde620d6958366dda5237eec4850a3396a
DevJoao-00 <victorjoao19921210@gmail.com>
2026-06-23 13:44:00 +0000

    .

--- fup/mercantil/src/go/draft.go
+++ fup/mercantil/src/go/draft.go
@@ -1,5 +1,56 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var rodada int
+
+    fmt.Scan(&rodada)
+
+    produtos := make([]float64, rodada)
+    chute := make([]float64, rodada)
+    escolhas := make([]string, rodada)
+
+        for i := 0; i < rodada; i++{
+            fmt.Scan(&produtos[i])
+        }
+
+        for i := 0; i < rodada; i++{
+            fmt.Scan(&chute[i])
+        }
+        
+        for i := 0; i < rodada; i++{
+            fmt.Scan(&escolhas[i])
+        }
+
+        jog1 := 0
+        jog2 := 0
+
+            for i := 0; i < rodada; i++{
+                if chute[i] == produtos[i]{
+                    jog1++
+                        continue
+                }
+
+                if escolhas[i] == "M"{
+                    if produtos[i] > chute[i]{
+                        jog2++
+                    } else {
+                        jog1++
+                    }
+                }
+                if escolhas[i] == "m"{
+                    if produtos[i] < chute[i]{
+                        jog2++
+                    }else {
+                        jog1++
+                    }
+                }
+            }
+
+            if jog1 > jog2{
+                fmt.Println("primeiro")
+            } else if jog1 < jog2 {
+                fmt.Println("segundo")
+            } else{
+                fmt.Println("empate")
+            }
 }
