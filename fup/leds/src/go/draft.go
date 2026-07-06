f108ab7deb86168bee3119c4624d9ec7d43151c0
DevJoao-00 <victorjoao19921210@gmail.com>
2026-07-06 20:29:08 +0000

    .

---
+++ fup/leds/src/go/draft.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main() {
+    var n int
+
+    fmt.Scan(&n)
+
+    leds := []int{6,2,5,5,4,5,6,3,7,6}
+
+    for i := 0; i < n; i++{
+        var num string
+        fmt.Scan(&num)
+
+        total := 0
+
+        for _, c := range num{
+            total += leds[c - '0']
+        }
+            fmt.Printf("%d leds\n", total)
+    }
+}
